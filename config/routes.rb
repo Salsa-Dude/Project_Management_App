a6c30699976402f2f267cae92b213b6af6af565d
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-12 15:47:37 -0500

    Resetting

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :collaborators
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :HiBeth
 end
