6c670b266bf902dbca4139fab0f4a8ff0e24626a
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-12 17:34:01 -0500

    Set session, root, and User.new

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   resources :projects
   resources :collaborators
   resources :users
+  root 'application#index'
+  get '/login' => 'sessions#new'
+  # get '/signup' => 'users#new'
+  post '/login' =>'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
