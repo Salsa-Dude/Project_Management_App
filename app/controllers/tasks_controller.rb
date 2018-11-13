6364a275f748a3a0973888c467bdf917778e5b05
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-11-13 09:45:57 -0500

    projects and tasks belong to current user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 
   def index
     @current_user = User.find_by(id: session[:user_id])
-    @tasks = @current_user.projects.collect {|t| t.name}
+    @tasks = @current_user.user_task_names
   end
 
   def show
