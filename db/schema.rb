99bfdb20a41274832fe1e563abc650066539a901
Joseph Arias <arias.joseph.a@gmail.com>
2019-02-11 08:51:37 -0500

    Switch to Postgresql and upgraded ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_14_123135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "collaborators", force: :cascade do |t|
     t.integer "user_id"
     t.integer "project_id"
